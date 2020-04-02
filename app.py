1ec7c4edf858113bde0e22e8cc74f74805200259
Maria Lobillo Santos <maria.lobillo.santos@gmail.com>
2020-04-02 20:18:49 +0200

    first commit

---
+++ app.py
@@ -0,0 +1,46 @@
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+stores = [
+    {
+        "name": "My wonderful Store",
+        "items": [
+            {
+                "name": "My Item",
+                "price": 15.99
+            }
+        ]
+    }
+]
+
+# POST - used to receive data
+# GET - used to send data back only
+
+# POST /store data: {name:}
+@app.route('/store', methods=['POST'])
+def create_store():
+    pass
+
+# GET /store/<sting:name>
+@app.route('/store/<string:name>')
+def get_store(name):
+    pass
+
+# GET /store
+@app.route('/store')
+def get_stores():
+    return jsonify({'stores': stores})
+
+# POST /store/<string:name/item {name:, price:}
+@app.route('/store/<string:name>/item', methods=['POST'])
+def create_item_in_store(name):
+    pass
+
+# GET /store/<string:name>/item
+@app.route('/store/<string:name>/item')
+def get_in_store(name):
+    pass
+
+
+app.run(port=5000)
